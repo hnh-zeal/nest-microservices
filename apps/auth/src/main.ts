9d52155fd4043f7ab1f1ada856027ff0e8a11fe9
Htet Naing Hein <htetnainghein2001@gmail.com>
2025-01-22 12:37:26 +0700

    feat: passport local strategy

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3001);
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT') ?? 3001);
 }
 void bootstrap();
